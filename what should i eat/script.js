9ca93b73ec4bdd7fa68f052b08e99b74c7d092b0
Seraphina <124868030+SpookySaturnCat@users.noreply.github.com>
2026-08-30 16:42:17 -0400

    disabled button start of js
    not much

--- what should i eat/script.js
+++ what should i eat/script.js
@@ -12,3 +12,31 @@ fetch("meals.json")
     .catch(error => {
         console.error("Could not load meal data:", error);
     })
+
+// DOM elements
+const cuisineButtons = document.querySelectorAll("[data-cuisine]");
+const effortButtons = document.querySelectorAll("[data-effort]");
+const budgetButtons = document.querySelectorAll("[data-budget]");
+const decideButton = document.querySelector("#decideButton");
+const mealName = document.querySelector("#mealName");
+const mealTime = document.querySelector("#mealTime");
+const mealBudget = document.querySelector("#mealBudget");
+const mealEffort = document.querySelector("#mealEffort");
+const ingredientsList = document.querySelector("#ingredients");
+
+// Load meal data
+fetch("meals.json")
+    .then(response => response.json())
+
+    .then(data => {
+        meals = data;
+        // json has loaded and the user can now use the decide button
+        decideButton.disabled = false;
+    })
+
+    .catch(error => {
+        console.error(
+            "Could not load meal data:",
+            error
+        );
+    });
